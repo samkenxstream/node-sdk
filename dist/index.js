29cf280ee1197ef39aff7513be3170763102c404
Djordje Lukic <djordje.lukic@docker.com>
2020-06-12 15:46:17 +0200

    Default socket path for win and others
    Signed-off-by: Djordje Lukic <djordje.lukic@docker.com>

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Streams = exports.Compose = exports.Contexts = exports.Containers = void 0;
+const tslib_1 = require("tslib");
+const os = tslib_1.__importStar(require("os"));
 const grpc_js_1 = require("@grpc/grpc-js");
 const containers_grpc_pb_1 = require("./protos/containers/v1/containers_grpc_pb");
 const contexts_grpc_pb_1 = require("./protos/contexts/v1/contexts_grpc_pb");
 const compose_grpc_pb_1 = require("./protos/compose/v1/compose_grpc_pb");
 const streams_grpc_pb_1 = require("./protos/streams/v1/streams_grpc_pb");
-// ~/Library/Containers/com.docker.docker/Data/cli-api.sock
-const addr = 'unix:///tmp/backend.sock';
+let addr = 'unix:///./pipe/dockerCliApi';
+if (os.platform() !== 'win32') {
+    const homeDir = os.homedir();
+    addr = `unix://${homeDir}/Library/Containers/com.docker.docker/Data/docker-cli-api.sock`;
+}
 class Containers extends containers_grpc_pb_1.ContainersClient {
     constructor(address = addr) {
         super(address, grpc_js_1.credentials.createInsecure());
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
+import * as os from 'os';
+
 import { credentials } from '@grpc/grpc-js';
 import { ContainersClient } from './protos/containers/v1/containers_grpc_pb';
 import { ContextsClient } from './protos/contexts/v1/contexts_grpc_pb';
 import { ComposeClient } from './protos/compose/v1/compose_grpc_pb';
 import { StreamingClient } from './protos/streams/v1/streams_grpc_pb';
 
-// ~/Library/Containers/com.docker.docker/Data/cli-api.sock
-const addr = 'unix:///tmp/backend.sock';
-
+let addr = 'unix:///./pipe/dockerCliApi';
+if (os.platform() !== 'win32') {
+  const homeDir = os.homedir();
+  addr = `unix://${homeDir}/Library/Containers/com.docker.docker/Data/docker-cli-api.sock`;
+}
 export class Containers extends ContainersClient {
   constructor(address: string = addr) {
     super(address, credentials.createInsecure());
