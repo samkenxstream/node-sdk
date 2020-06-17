e41efd43cc3628992b3d8bce130c654641d1c093
Djordje Lukic <djordje.lukic@docker.com>
2020-06-17 09:38:55 +0200

    Remove unused dependencies
    Signed-off-by: Djordje Lukic <djordje.lukic@docker.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as os from 'os';
+import { homedir, platform } from 'os';
 
 import { credentials } from '@grpc/grpc-js';
 import { ContainersClient } from './protos/containers/v1/containers_grpc_pb';
@@ -7,8 +7,8 @@ import { ComposeClient } from './protos/compose/v1/compose_grpc_pb';
 import { StreamingClient } from './protos/streams/v1/streams_grpc_pb';
 
 let addr = 'unix:////./pipe/dockerCliApi';
-if (os.platform() !== 'win32') {
-  const homeDir = os.homedir();
+if (platform() !== 'win32') {
+  const homeDir = homedir();
   addr = `unix://${homeDir}/Library/Containers/com.docker.docker/Data/docker-cli-api.sock`;
 }
 export class Containers extends ContainersClient {
