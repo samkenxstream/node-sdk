334e6939e07397f3713eb9e612847713b45cba00
Chris Crone <christopher.crone@docker.com>
2020-09-24 10:09:17 +0200

    license: Update headers
    Signed-off-by: Chris Crone <christopher.crone@docker.com>

--- src/streams.ts
+++ src/streams.ts
@@ -1,5 +1,5 @@
 /*
-   Copyright 2020 The Authors
+   Copyright 2020 Docker CLI JavaScript SDK authors
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
